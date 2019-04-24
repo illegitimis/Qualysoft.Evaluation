4eec87f305df5c4b71d6effe526dfc1f88d74475
illegitimis <illegitimis@gmail.com>
2019-04-24 10:34:21 +0300

    formatters protobuf-net

---
+++ Formatters/Protobuf/ProtobufNetFormatterMvcBuilderExtensions.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Formatters.Protobuf
+{
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+    using System.Net.Http.Headers;
+    using System.Text;
+
+    public static class ProtobufNetFormatterMvcBuilderExtensions
+    {
+        public static IMvcBuilder AddProtobufFormatters(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return AddProtobufFormatters(builder, protobufFormatterOptionsConfiguration: null);
+        }
+
+        public static IMvcCoreBuilder AddProtobufFormatters(this IMvcCoreBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return AddProtobufFormatters(builder, protobufFormatterOptionsConfiguration: null);
+        }
+
+        public static IMvcBuilder AddProtobufFormatters(this IMvcBuilder builder, Action<ProtobufFormatterOptions> protobufFormatterOptionsConfiguration)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            var protobufFormatterOptions = new ProtobufFormatterOptions();
+            protobufFormatterOptionsConfiguration?.Invoke(protobufFormatterOptions);
+
+            foreach (var extension in protobufFormatterOptions.SupportedExtensions)
+            {
+                foreach (var contentType in protobufFormatterOptions.SupportedContentTypes)
+                {
+                    builder.AddFormatterMappings(m => m.SetMediaTypeMappingForFormat(extension, 
+                        //new MediaTypeHeaderValue(contentType)
+                        contentType
+                        ));
+                }
+            }
+
+            builder.AddMvcOptions(options => options.InputFormatters.Add(new ProtobufNetInputFormatter(protobufFormatterOptions)));
+            builder.AddMvcOptions(options => options.OutputFormatters.Add(new ProtobufNetOutputFormatter(protobufFormatterOptions)));
+
+            return builder;
+        }
+
+        public static IMvcCoreBuilder AddProtobufFormatters(this IMvcCoreBuilder builder, Action<ProtobufFormatterOptions> protobufFormatterOptionsConfiguration)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            var protobufFormatterOptions = new ProtobufFormatterOptions();
+            protobufFormatterOptionsConfiguration?.Invoke(protobufFormatterOptions);
+
+            foreach (var extension in protobufFormatterOptions.SupportedExtensions)
+            {
+                foreach (var contentType in protobufFormatterOptions.SupportedContentTypes)
+                {
+                    builder.AddFormatterMappings(m => m.SetMediaTypeMappingForFormat(extension, 
+                        //new MediaTypeHeaderValue(contentType)
+                        contentType
+                        ));
+                }
+            }
+
+            builder.AddMvcOptions(options => options.InputFormatters.Add(new ProtobufNetInputFormatter(protobufFormatterOptions)));
+            builder.AddMvcOptions(options => options.OutputFormatters.Add(new ProtobufNetOutputFormatter(protobufFormatterOptions)));
+
+            return builder;
+        }
+    }
+}
---
+++ Formatters/Protobuf/ProtobufNetOutputFormatter.cs
@@ -0,0 +1,47 @@
+﻿namespace Formatters.Protobuf
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Formatters;
+    using Microsoft.Net.Http.Headers;
+    using ProtoBuf.Meta;
+
+    public class ProtobufNetOutputFormatter : OutputFormatter
+    {
+        // private readonly ProtobufFormatterOptions _options;
+
+        public ProtobufNetOutputFormatter(ProtobufFormatterOptions protobufFormatterOptions)
+        {
+            ContentType = "application/x-protobuf";
+            var _options = protobufFormatterOptions ?? throw new ArgumentNullException(nameof(protobufFormatterOptions));
+            foreach (var contentType in _options.SupportedContentTypes)
+            {
+                SupportedMediaTypes.Add(new MediaTypeHeaderValue(contentType));
+            }
+        }
+
+        private static Lazy<RuntimeTypeModel> model = new Lazy<RuntimeTypeModel>(CreateTypeModel);
+
+        public string ContentType { get; private set; }
+
+        public static RuntimeTypeModel Model
+        {
+            get { return model.Value; }
+        }
+
+        private static RuntimeTypeModel CreateTypeModel()
+        {
+            var typeModel = TypeModel.Create();
+            typeModel.UseImplicitZeroDefaults = false;
+            return typeModel;
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+        {
+            var response = context.HttpContext.Response;
+
+            Model.Serialize(response.Body, context.Object);
+            return Task.FromResult(response);
+        }
+    }
+}
